00a0823b524ef31376ece6ecb851127357018e66
Rhilip <rhilipruan@gmail.com>
2020-07-18 17:01:02 +0800

    fix: 单例锁下，双击exe文件不显示已经隐藏到任务栏里的程序

--- src/background.ts
+++ src/background.ts
@@ -98,7 +98,8 @@ if (!gotTheLock) {
     // Someone tried to run a second instance, we should focus our window.
     if (win) {
       if (win.isMinimized()) win.restore()
-      win.focus()
+      if (!win.isVisible()) win.show()
+      if (!win.isFocused()) win.focus()
     }
   })
 
