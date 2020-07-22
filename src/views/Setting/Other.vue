822ed8ff5353151bf89d16daa07979fe13d644c5
Rhilip <rhilipruan@gmail.com>
2020-07-22 11:29:38 +0800

    feat: 暴露DevTools入口

--- src/views/Setting/Other.vue
+++ src/views/Setting/Other.vue
@@ -15,11 +15,31 @@
                     // TODO 微信推送模板
                 </el-tab-pane>
                 <el-tab-pane label="其他" name="other">
-                    // 此处请不要点击
-                    <el-button type="warning"
-                               @click="test">
-                        点击测试
-                    </el-button>
+                    <el-row>
+                        <el-col :span="4">
+                            <el-button type="warning"
+                                       @click="openDevTools">
+                                调试工具
+                            </el-button>
+                        </el-col>
+                        <el-col :span="20">
+                            打开调试工具（如果你确定是高手，或者想要观察软件是怎么工作的）<br>
+                            那么你可以点击此处打开Developer Tools面板
+                        </el-col>
+                    </el-row>
+                    <!--
+                    <el-row>
+                        <el-col :span="4">
+                            <el-button type="warning"
+                                       @click="text">
+                                点击测试
+                            </el-button>
+                        </el-col>
+                        <el-col :span="20">
+                            这个没啥用，用来临时测试些方法时候正常的时候使用
+                        </el-col>
+                    </el-row>
+                    -->
                 </el-tab-pane>
             </el-tabs>
         </el-card>
@@ -29,6 +49,7 @@
 
 <script>
 import Normal from "../../components/Setting/Other/Normal";
+import {remote} from 'electron'
 export default {
   name: 'Other',
   components: {Normal},
@@ -38,9 +59,13 @@ export default {
     }
   },
   methods: {
-    async test() {
-    }
+    openDevTools() {
+      remote.getCurrentWebContents().openDevTools()
+    },
 
+    async test() {
+
+    }
   }
 }
 </script>
